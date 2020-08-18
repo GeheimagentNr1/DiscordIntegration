50c7e15c835beab7be24cde9079428b41191ba11
GeheimagentNr1 <geheimelache@hotmail.com>
2020-08-18 21:11:06 +0200

    Ported to 1.16.2

--- src/main/java/de/geheimagentnr1/discordintegration/commands/discord/moremobgriefingoptions/MobgriefingCommand.java
+++ src/main/java/de/geheimagentnr1/discordintegration/commands/discord/moremobgriefingoptions/MobgriefingCommand.java
@@ -2,7 +2,7 @@ package de.geheimagentnr1.discordintegration.commands.discord.moremobgriefingopt
 
 import de.geheimagentnr1.discordintegration.commands.discord.CommandHandler;
 import de.geheimagentnr1.discordintegration.net.DiscordNet;
-import de.geheimagentnr1.moremobgriefingoptions.config.Config;
+import de.geheimagentnr1.moremobgriefingoptions.config.MainConfig;
 import net.minecraft.command.CommandSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.GameRules;
@@ -27,9 +27,9 @@ public class MobgriefingCommand extends CommandHandler {
 		message.append( String.format( "**Mobgriefing Settings**%n" ) );
 		message.append( String.format( "vanilla mobGriefing = %s%n",
 			server.getGameRules().getBoolean( GameRules.MOB_GRIEFING ) ) );
-		for( int i = 0; i < Config.OPTIONS.length; i++ ) {
-			message.append( String.format( "%s = %s%n", Config.OPTIONS[i].getKey(),
-				Config.OPTIONS[i].getValue().name().toLowerCase( Locale.ENGLISH ) ) );
+		for( int i = 0; i < MainConfig.OPTIONS.length; i++ ) {
+			message.append( String.format( "%s = %s%n", MainConfig.OPTIONS[i].getKey(),
+				MainConfig.OPTIONS[i].getValue().name().toLowerCase( Locale.ENGLISH ) ) );
 		}
 		DiscordNet.sendMessage( message.toString() );
 	}
