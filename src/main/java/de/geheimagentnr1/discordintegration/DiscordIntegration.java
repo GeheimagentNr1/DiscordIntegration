fc1289e200c5c1a8e2dfb661b8b75666b27a8e62
GeheimagentNr1 <geheimelache@hotmail.com>
2022-07-19 16:11:09 +0200

    Update forge version

--- src/main/java/de/geheimagentnr1/discordintegration/DiscordIntegration.java
+++ src/main/java/de/geheimagentnr1/discordintegration/DiscordIntegration.java
@@ -1,11 +1,9 @@
 package de.geheimagentnr1.discordintegration;
 
 import de.geheimagentnr1.discordintegration.config.ServerConfig;
-import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.network.NetworkConstants;
 
 
 @SuppressWarnings( "UtilityClassWithPublicConstructor" )
@@ -18,12 +16,5 @@ public class DiscordIntegration {
 	public DiscordIntegration() {
 		
 		ModLoadingContext.get().registerConfig( ModConfig.Type.SERVER, ServerConfig.CONFIG );
-		ModLoadingContext.get().registerExtensionPoint(
-			IExtensionPoint.DisplayTest.class,
-			() -> new IExtensionPoint.DisplayTest(
-				() -> NetworkConstants.IGNORESERVERONLY,
-				( remote, isServer ) -> true
-			)
-		);
 	}
 }
